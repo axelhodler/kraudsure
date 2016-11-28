cb96a28c40f457dea48293eefbe6e15a4a088308
Axel Hodler <derxor@gmail.com>
2016-11-29 00:19:50 +0100

    contract creator is the insured

--- contracts/KrowdSure.sol
+++ contracts/KrowdSure.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.2;
 
 contract KrowdSure {
+  address public insured;
   string public content;
+
   function KrowdSure(string _content) {
     content = _content;
+    insured = msg.sender;
   }
 }
--- test/krowdsure.js
+++ test/krowdsure.js
@@ -6,4 +6,11 @@ contract('KrowdSure', function () {
       assert.equal(content, 'asdf');
     });
   });
+  it('sets the contract creator as the insured party', function () {
+    var krowd = KrowdSure.deployed();
+
+    return krowd.insured.call().then(function (insured) {
+      assert.equal(insured, '0x6ad2f06fad39bfa860769c35fc53027ab1952061');
+    });
+  });
 });
