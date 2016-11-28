034c0bf0673aa1272e14d539e5f1a58a935dcb23
Axel Hodler <derxor@gmail.com>
2016-11-29 00:21:34 +0100

    refactor tests

--- test/krowdsure.js
+++ test/krowdsure.js
@@ -1,14 +1,17 @@
 contract('KrowdSure', function () {
-  it('takes the documenthash as an argument', function () {
-    var krowd = KrowdSure.deployed();
+  var krowd;
 
+  beforeEach(function() {
+    krowd = KrowdSure.deployed();
+  });
+
+  it('takes the documenthash as an argument', function () {
     return krowd.content.call().then(function (content) {
       assert.equal(content, 'asdf');
     });
   });
-  it('sets the contract creator as the insured party', function () {
-    var krowd = KrowdSure.deployed();
 
+  it('sets the contract creator as the insured party', function () {
     return krowd.insured.call().then(function (insured) {
       assert.equal(insured, '0x6ad2f06fad39bfa860769c35fc53027ab1952061');
     });
