da8663e644b0e96b1cc21e7993ff8c4c5c3e460c
Axel Hodler <derxor@gmail.com>
2016-11-29 16:52:37 +0100

    contract service api

--- app/src/cases/services/ContractService.js
+++ app/src/cases/services/ContractService.js
@@ -1,7 +1,17 @@
 function ContractService() {
-  this.foo = function() {
-    return 'foo';
-  }
+  var contract = Chaininsurance.deployed();
+
+  this.fundedAmount = function() {
+  };
+
+  this.fund = function(amount) {
+  };
+
+  this.issueClaim = function() {
+  };
+
+  this.refund = function() {
+  };
 }
 
 export default [ContractService]
