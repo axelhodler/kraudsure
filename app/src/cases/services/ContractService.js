9650ddc464313dbfc08247c318c8b1296803ecbb
Axel Hodler <derxor@gmail.com>
2016-11-29 19:54:14 +0100

    create contract interface

--- app/src/cases/services/ContractService.js
+++ app/src/cases/services/ContractService.js
@@ -1,16 +1,38 @@
 function ContractService() {
   var contract = Chaininsurance.deployed();
 
+  function currentUserAddress() {
+    return web3.eth.coinbase;
+  }
+
+  function toWei(amount) {
+    return web3.toWei(amount, 'ether');
+  }
+
   this.fundedAmount = function() {
+    return contract.fundedAmount.call();
+  };
+
+  this.deposit = function(amount) {
+    return contract.deposit.sendTransaction({
+      from: currentUserAddress(),
+      value: toWei(amount)
+    })
   };
 
   this.fund = function(amount) {
+    return contract.fund.sendTransaction({
+      from: currentUserAddress(),
+      value: toWei(amount)
+    });
   };
 
   this.issueClaim = function() {
+    return contract.issueClaim();
   };
 
   this.refund = function() {
+    return contract.refund();
   };
 }
 
