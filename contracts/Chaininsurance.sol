12c59d1f21a544b3a2f39545f40cc89c0a116e06
Axel Hodler <derxor@gmail.com>
2016-11-30 09:40:26 +0100

    no need to fully fund for now

--- contracts/Chaininsurance.sol
+++ contracts/Chaininsurance.sol
@@ -21,13 +21,11 @@ contract Chaininsurance {
   function fund() payable {
     fundedAmount += msg.value;
     funders[msg.sender] += msg.value;
-    if (fundedAmount >= insuredAmount) {
-      var profit = (funders[msg.sender] / insuredAmount) * depositedAmount;
-      if (!msg.sender.send(profit)) {
-        throw;
-      } else {
-        depositedAmount -= profit;
-      }
+    var profit = (funders[msg.sender] / insuredAmount) * depositedAmount;
+    if (!msg.sender.send(profit)) {
+      throw;
+    } else {
+      depositedAmount -= profit;
     }
   }
 
