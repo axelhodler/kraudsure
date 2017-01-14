a0c3c7783deaa1c1085fa27e50e29a712a88f9d8
Axel Hodler <derxor@gmail.com>
2017-01-14 15:22:47 +0100

    never stored these

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 module.exports = function(deployer) {
   var ONE_SECOND = 1;
   deployer.deploy(Chaininsurance,
-    'asdf',
+    'insurance for x',
     1000,
-    '0xd35bb587f9936d701603523858c9e2ddf18012bf',
+    '0x354990de9386772900C6B257EC0b8dCC5AF8BFba',
     Math.round(+new Date() / 1000) + ONE_SECOND);
 };
